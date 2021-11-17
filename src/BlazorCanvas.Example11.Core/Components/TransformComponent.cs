3b837c2fd07b9f611567018746100deb92ee19da
Elliott Record <elliottrecord@gmail.com>
2021-11-17 21:47:05 +0000

    Added files to src/

---
+++ src/BlazorCanvas.Example11.Core/Components/TransformComponent.cs
@@ -0,0 +1,45 @@
+using System.Drawing;
+using System.Threading.Tasks;
+using BlazorCanvas.Example11.Core.Utils;
+
+namespace BlazorCanvas.Example11.Core.Components
+{
+    public class TransformComponent : BaseComponent
+    {
+        private readonly Transform _local = Transform.Identity();
+        private readonly Transform _world = Transform.Identity();
+
+        private TransformComponent(GameObject owner) : base(owner)
+        {
+        }
+
+        public override ValueTask Update(GameContext game)
+        {
+            _world.Clone(_local);
+
+            if (null != Owner.Parent && Owner.Parent.Components.TryGet<TransformComponent>(out var parentTransform))
+                _world.Position = _local.Position + parentTransform.World.Position;
+            return new ValueTask();
+        }
+
+        public void SetRandomPosition(double canvasWidth, double canvasHeight)
+        {
+            var rx = MathUtils.Random.NextDouble(0, .35, .65, 1);
+            var tx = MathUtils.Normalize(rx, 0, 1, -1, 1);
+            Local.Position.X = (float)(tx * canvasWidth / 2 + canvasWidth / 2);
+
+            var ry = MathUtils.Random.NextDouble(0, .35, .65, 1);
+            var ty = MathUtils.Normalize(ry, 0, 1, -1, 1);
+            Local.Position.Y = (float)(ty * canvasHeight / 2 + canvasHeight / 2);
+        }
+
+        public void SetPosition(Point position)
+        {
+            Local.Position.X = position.X;
+            Local.Position.Y = position.Y;
+        }
+
+        public Transform Local => _local;
+        public Transform World => _world;
+    }
+}
