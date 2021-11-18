9f29187717b1ff2e7f91b25bade5ac54289b7f58
Elliott Record <elliottrecord@gmail.com>
2021-11-18 21:10:35 +0000

    Removed travel concerns from CarBrainAutomatic

--- src/BlazorCanvas.Sandbox/Game/Components/CarBrainAutomatic.cs
+++ src/BlazorCanvas.Sandbox/Game/Components/CarBrainAutomatic.cs
@@ -1,63 +1,16 @@
-using System.Threading.Tasks;
 using BlazorCanvas.Core;
-using BlazorCanvas.Sandbox.Core;
 
 namespace BlazorCanvas.Example11.Game.Components
 {
-
     public class CarBrainAutomatic : CarBrain
     {
-
         private CarObject Parent => Owner as CarObject;
 
         public CarBrainAutomatic(GameObject owner) : base(owner)
         {
             _speed = 0.1500f;
+            Parent.Stopped = false;
         }
 
-        public override ValueTask Update(GameContext game)
-        {
-            if (Parent.State == CarStateEnum.Northbound)
-            {
-                TravelNorth(game);
-            }
-            if (Parent.State == CarStateEnum.Eastbound)
-            {
-
-                TravelEast(game);
-            }
-            if (Parent.State == CarStateEnum.Southbound)
-            {
-
-                TravelSouth(game);
-            }
-            if (Parent.State == CarStateEnum.Westbound)
-            {
-
-                TravelWest(game);
-            }
-            if (Parent.State == CarStateEnum.NorthEast)
-            {
-
-                TravelNorthEast(game);
-            }
-            if (Parent.State == CarStateEnum.NorthWest)
-            {
-
-                TravelNorthWest(game);
-            }
-            if (Parent.State == CarStateEnum.SouthEast)
-            {
-
-                TravelSouthEast(game);
-            }
-            if (Parent.State == CarStateEnum.SouthWest)
-            {
-
-                TravelSouthWest(game);
-            }
-
-            return new ValueTask();
-        }
     }
 }
