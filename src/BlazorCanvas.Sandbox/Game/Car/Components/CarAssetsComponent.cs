1b8d3cd2daa95dd44ba9ca06cc92071b7094f0b6
Elliott Record <elliottrecord@gmail.com>
2021-11-28 13:12:13 +0000

    Updated to handle CarState.Stopped

--- src/BlazorCanvas.Sandbox/Game/Car/Components/CarAssetsComponent.cs
+++ src/BlazorCanvas.Sandbox/Game/Car/Components/CarAssetsComponent.cs
@@ -4,16 +4,17 @@ using BlazorCanvas.Core;
 using BlazorCanvas.Core.Assets;
 using BlazorCanvas.Core.Components;
 using BlazorCanvas.Sandbox.Core;
+using BlazorCanvas.Sandbox.Game.GameObjects;
 
-namespace BlazorCanvas.Example11.Game.Components
+namespace BlazorCanvas.Sandbox.Game.Components
 {
     public class CarAssetsComponent : BaseComponent
     {
         private SpriteRenderComponent _spriteRenderComponent;
         private BoundingBoxComponent _spriteBoundingBoxComponent;
-        private Dictionary<CarStateEnum, string> _carStateAssetDictionary;
+        private Dictionary<CarState, string> _carStateAssetDictionary;
 
-        private CarStateEnum CarState => (Owner as CarObject).State;
+        private CarState CarState => (Owner as CarObject).State;
         private string _selectedAssetPath;
 
         public CarAssetsComponent(GameObject owner) : base(owner)
@@ -21,32 +22,50 @@ namespace BlazorCanvas.Example11.Game.Components
             _spriteRenderComponent = owner.Components.Get<SpriteRenderComponent>();
             _spriteBoundingBoxComponent = owner.Components.Get<BoundingBoxComponent>();
 
-            _carStateAssetDictionary = new Dictionary<CarStateEnum, string>()
+            _carStateAssetDictionary = new Dictionary<CarState, string>()
             {
-                {CarStateEnum.Northbound, "assets/sedanSports_N.png"},
-                {CarStateEnum.Southbound, "assets/sedanSports_S.png"},
-                {CarStateEnum.Eastbound, "assets/sedanSports_E.png"},
-                {CarStateEnum.Westbound, "assets/sedanSports_W.png"},
-                {CarStateEnum.NorthEast, "assets/sedanSports_NE.png"},
-                {CarStateEnum.NorthWest, "assets/sedanSports_NW.png"},
-                {CarStateEnum.SouthEast, "assets/sedanSports_SE.png"},
-                {CarStateEnum.SouthWest, "assets/sedanSports_SW.png"}
+                {CarState.Northbound, "assets/sedanSports_N.png"},
+                {CarState.Southbound, "assets/sedanSports_S.png"},
+                {CarState.Eastbound, "assets/sedanSports_E.png"},
+                {CarState.Westbound, "assets/sedanSports_W.png"},
+                {CarState.NorthEast, "assets/sedanSports_NE.png"},
+                {CarState.NorthWest, "assets/sedanSports_NW.png"},
+                {CarState.SouthEast, "assets/sedanSports_SE.png"},
+                {CarState.SouthWest, "assets/sedanSports_SW.png"},
             };
+
+        }
+
+        public override void OnStart(GameContext game)
+        {
+            base.OnStart(game);
+
+            // Set initial asset
+            _selectedAssetPath = _carStateAssetDictionary[CarState.Eastbound];
+            updateCarAsset(game, _selectedAssetPath);
         }
 
         public override ValueTask Update(GameContext game)
         {
-            var carStateAssetPath = _carStateAssetDictionary[CarState];
-            if (_selectedAssetPath != carStateAssetPath)
-            {
-                _selectedAssetPath = carStateAssetPath;
 
-                var assetsResolver = game.GetService<AssetsResolver>();
-                var carSprite = assetsResolver.Get<Sprite>(carStateAssetPath);
-                _spriteRenderComponent.Sprite = carSprite;
-                _spriteBoundingBoxComponent.SetSize(carSprite.Bounds.Size);
+            if (CarState != CarState.Stopped)
+            {
+                var carStateAssetPath = _carStateAssetDictionary[CarState];
+                if (_selectedAssetPath != carStateAssetPath)
+                {
+                    _selectedAssetPath = carStateAssetPath;
+                    updateCarAsset(game, _selectedAssetPath);
+                }
             }
             return new ValueTask();
+        }
+
+        private void updateCarAsset(GameContext game, string assetPath)
+        {
+            var assetsResolver = game.GetService<AssetResolver>();
+            var carSprite = assetsResolver.Get<Sprite>(assetPath);
+            _spriteRenderComponent.Sprite = carSprite;
+            _spriteBoundingBoxComponent.SetSize(carSprite.Bounds.Size);
         }
     }
 }
