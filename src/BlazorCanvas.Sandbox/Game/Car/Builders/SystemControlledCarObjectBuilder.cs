fab999b697d3e8247608913f202bdaacd6e430d4
Elliott Record <elliottrecord@gmail.com>
2021-11-28 20:06:35 +0000

    Set TravelToTargetStrategy in builder

--- src/BlazorCanvas.Sandbox/Game/Car/Builders/SystemControlledCarObjectBuilder.cs
+++ src/BlazorCanvas.Sandbox/Game/Car/Builders/SystemControlledCarObjectBuilder.cs
@@ -1,16 +1,18 @@
+using BlazorCanvas.Sandbox.Core.Interfaces;
 using BlazorCanvas.Sandbox.Game.Components;
+using BlazorCanvas.Sandbox.Game.GameObjects;
+using BlazorCanvas.Sandbox.Game.Strategies;
 
 namespace BlazorCanvas.Sandbox.Game.Builders
 {
     public class SystemControlledCarObjectBuilder : BaseCarObjectBuilder
     {
-        protected TravelToTargetComponent _travelToTargetComponent => CarObject.Components.Get<TravelToTargetComponent>();
-        public SystemControlledCarObjectBuilder()
-        {
-        }
+        private ITravelToTargetPositionComponent _travelToTargetComponent => CarObject.Components.Get<TravelToTargetPositionComponent>();
         public override void SetBehaviour()
         {
-            CarObject.Components.Add<TravelToTargetComponent>();
+            CarObject.Components.Add<TravelToTargetPositionComponent>();
+            _travelToTargetComponent.TravelToTargetPositionStrategy = new CarToTargetPositionStrategy(CarObject);
         }
+
     }
 }
