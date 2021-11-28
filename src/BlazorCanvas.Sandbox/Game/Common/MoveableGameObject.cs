478fa5241ad8b713477c0b97dec9587db2c724e8
Elliott Record <elliottrecord@gmail.com>
2021-11-28 20:07:24 +0000

    Removed TargetPosition callback from MoveableGameObject

--- src/BlazorCanvas.Sandbox/Game/Common/MoveableGameObject.cs
+++ src/BlazorCanvas.Sandbox/Game/Common/MoveableGameObject.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Drawing;
 using System.Numerics;
 using BlazorCanvas.Core;
 using BlazorCanvas.Core.Components;
@@ -9,7 +7,6 @@ namespace BlazorCanvas.Sandbox.Game.Components
     public class MoveableGameObject : GameObject
     {
         private TransformComponent _transformComponent => Components.Get<TransformComponent>();
-        private Point _position;
         public Vector2 Position
         {
             get => Components.Get<TransformComponent>().World.Position;
@@ -21,25 +18,7 @@ namespace BlazorCanvas.Sandbox.Game.Components
             Components.Add<TransformComponent>();
         }
 
-        public void SetPosition(Vector2 newPosition)
-        {
-            var transformComponent = Components.Get<TransformComponent>();
-            transformComponent.SetPosition(newPosition);
-        }
-
-        public Func<Vector2> TargetPositionCallback
-        {
-            get => Components.Get<TravelToTargetComponent>()?.GetTargetPositionFunc;
-            set
-            {
-                var travelToTargetComponent = Components.Get<TravelToTargetComponent>();
-                if (travelToTargetComponent != null)
-                {
-                    travelToTargetComponent.GetTargetPositionFunc = value;
-                }
-            }
-        }
-
+        public void SetPosition(Vector2 newPosition) => _transformComponent.SetPosition(newPosition);
 
     }
 
